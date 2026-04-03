d4d7fe8d0c9875c85fa5ae0cab1c2cb1b5f50d26
ricciliao <riccix@163.com>
2026-04-03 16:34:14 +0800

    refactor: align response code with BE.

--- src/x/common/responseCodeEnum.ts
+++ src/x/common/responseCodeEnum.ts
@@ -1,11 +1,10 @@
 enum responseCodeEnum {
-    SUCCESS = "0000",
-    SECURITY_ERROR = "1000",
-    PARAMETER_ERROR = "2000",
-    CONCURRENT_ERROR = "3000",
-    REST_ERROR = "4000",
-    DATA_ERROR = "5000",
-    UNEXPECTED_ERROR = "9000",
+    SUCCESS = "0",
+    DATA_WARNING = "1",
+    PARAMETER_WARNING = "2",
+    REST_WARNING = "3",
+    SECURITY_ERROR = "8",
+    UNEXPECTED_ERROR = "9",
     BROKEN_HTTP = "-1",
 }
 
--- src/x/common/responseCodeMap.ts
+++ src/x/common/responseCodeMap.ts
@@ -3,11 +3,10 @@ import {responseCodeEnum} from "./responseCodeEnum";
 
 const responseCodeMap: Record<responseCodeEnum, ResponseCode> = {
     [responseCodeEnum.SUCCESS]: {id: "0000", message: ""},
-    [responseCodeEnum.SECURITY_ERROR]: {id: "1000", message: ""},
-    [responseCodeEnum.PARAMETER_ERROR]: {id: "2000", message: ""},
-    [responseCodeEnum.CONCURRENT_ERROR]: {id: "3000", message: ""},
-    [responseCodeEnum.REST_ERROR]: {id: "4000", message: ""},
-    [responseCodeEnum.DATA_ERROR]: {id: "5000", message: ""},
+    [responseCodeEnum.DATA_WARNING]: {id: "1000", message: ""},
+    [responseCodeEnum.PARAMETER_WARNING]: {id: "2000", message: ""},
+    [responseCodeEnum.REST_WARNING]: {id: "3000", message: ""},
+    [responseCodeEnum.SECURITY_ERROR]: {id: "8000", message: ""},
     [responseCodeEnum.UNEXPECTED_ERROR]: {id: "9000", message: ""},
     [responseCodeEnum.BROKEN_HTTP]: {id: "-1", message: ""},
 }
